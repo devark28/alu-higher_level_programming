0206ae8289a50036ec33e99aa7e781e88b5f8b7d
devark28 <e.shimwa1@alustudent.com>
2024-10-22 21:44:52 +0200

    Make 101-remove_char_at.py Tue Oct 22 21:44:52 SAST 2024 executable

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 
-def remove_char_at(str="noy", n=1):
+def remove_char_at(str, n):
     res = ""
     for i in range(len(str)):
         if i != n:
             res += str[i]
     print(f"{res}")
     return res
+
+remove_char_at("Holberton", 3)
