cae100661ea102e5c6dacf6ca4ba816e959b388c
devark28 <e.shimwa1@alustudent.com>
2024-10-22 21:58:35 +0200

    Make 102-magic_calculation.py Tue Oct 22 21:58:35 SAST 2024 executable

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import dis
+
 
 def magic_calculation(a, b, c):
     if not a < b:
@@ -7,5 +7,3 @@ def magic_calculation(a, b, c):
     if not c > b:
         return a + b
     return (a * b) - c
-
-dis.dis(magic_calculation)
