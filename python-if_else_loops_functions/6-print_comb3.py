f9f8d91fdc78f1fe303197d9dd769cfee460b067
devark28 <e.shimwa1@alustudent.com>
2024-10-22 19:20:01 +0200

    Make 6-print_comb3.py Tue Oct 22 19:20:01 SAST 2024 executable

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for n in range(10):
     for m in range(n + 1, 10):
         print("{}{}".format(n , m),
-              end=", " if format("{}{}".format(n , m)) != "89" else "\n")
+              end=", " if format("{}{}".format(n, m)) != "89" else "\n")
