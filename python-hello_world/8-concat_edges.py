f3a8f59c713d9e3644c8d93da9042348e7c91a50
devark28 <e.shimwa1@alustudent.com>
2024-10-22 15:22:18 +0200

    Make 8-concat_edges.py Tue Oct 22 15:20:39 SAST 2024 executable

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# str = str[39:66] + str[106:111] + str[:6]
 i=0
 for c in str:
   print(f"{i}: {c}")
   i+=1
+str=str[39:66]+str[107:110]+str[0:5]
 print(str)
