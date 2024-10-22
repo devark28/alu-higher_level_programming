deed40536ce0684cad1101a53dabe022b0be56ac
devark28 <e.shimwa1@alustudent.com>
2024-10-22 20:45:09 +0200

    Make 8-uppercase.py Tue Oct 22 20:45:09 SAST 2024 executable

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,10 @@
 
 
 def uppercase(text):
+    charsBag = str(text)
     res = ""
-    for c in text:
-        if 'a' <= c <= 'z':
+    for c in charsBag:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
             translation = ord(c) - ord('a')
             res += chr(ord('A') + translation)
         else:
