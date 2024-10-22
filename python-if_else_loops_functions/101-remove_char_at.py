2ba3c1c9248a8b498bbe06951bd3f12c967756ab
devark28 <e.shimwa1@alustudent.com>
2024-10-22 21:44:05 +0200

    Make 101-remove_char_at.py Tue Oct 22 21:44:05 SAST 2024 executable

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-def remove_char_at(str, n):
-    res=""
+def remove_char_at(str="noy", n=1):
+    res = ""
     for i in range(len(str)):
         if i != n:
             res += str[i]
