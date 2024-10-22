c7063b3e7550d5df8cdd3e7361542c22ef12b563
devark28 <e.shimwa1@alustudent.com>
2024-10-22 19:03:44 +0200

    Make 6-print_comb3.py Tue Oct 22 19:03:44 SAST 2024 executable

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for n in range(10):
     for m in range(10):
         if (n * 10 + m) < (m * 10 + n):
-            print("{}".format((n * 10 + m)),
+            print("{}".format(str(n * 10 + m).zfill(2)),
                   end=(", " if (n * 10 + m) < 89 else "\n"))
         else:
-            print("{}".format((m * 10 + n)),
+            print("{}".format(str(m * 10 + n).zfill(2)),
                   end=(", " if (m * 10 + n) < 89 else "\n"))
