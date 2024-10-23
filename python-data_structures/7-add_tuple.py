ecb0335a8de8d29773383c47248559e23b0adae5
devark28 <e.shimwa1@alustudent.com>
2024-10-23 20:13:37 +0200

    Make 7-add_tuple.py Wed Oct 23 20:13:37 SAST 2024 executable

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,11 +2,11 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_x = (0, 0)
-    tuple_y = (0, 0)
-    for i in range(2):
-        tuple_x[i] = tuple_a[i] if i < len(tuple_a) else 0
-        tuple_y[i] = tuple_b[i] if i < len(tuple_b) else 0
+    tuple_x = (tuple_a[i] if i < len(tuple_a) else 0 for i in range(2))
+    tuple_y = (tuple_b[i] if i < len(tuple_b) else 0 for i in range(2))
+    # for i in range(2):
+    #     tuple_x[i] = tuple_a[i] if i < len(tuple_a) else 0
+    #     tuple_y[i] = tuple_b[i] if i < len(tuple_b) else 0
     return tuple(map(sum, zip(tuple_x, tuple_y)))
     
 print(add_tuple((1,), (1, 2, 3)))
