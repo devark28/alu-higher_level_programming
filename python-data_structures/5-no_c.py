c27a4343ef70334f538f49bdffaa6a6eb81702dc
devark28 <e.shimwa1@alustudent.com>
2024-10-23 19:02:47 +0200

    Make 5-no_c.py Wed Oct 23 19:02:43 SAST 2024 executable

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 
 
 def no_c(my_string):
-    for c in range(my_string.length):
+    for c in range(my_string.__len__()):
         if my_string[c] == 'c' or c == 'C':
             my_string[c] = ''
     return my_string
