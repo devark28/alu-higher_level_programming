2200a9908b529a534edea56f9f0204494a2aa7b4
devark28 <e.shimwa1@alustudent.com>
2024-10-23 10:45:01 +0200

    Make 2-args.py Wed Oct 23 10:45:01 SAST 2024 executable

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    param_len = len(argv) - 1
+    print(f"{param_len} {'argument' if param_len == 1 else 'arguments'}:")
+    for arg in argv[1:]:
+        print(f"{argv.index(arg)}: {arg}")
