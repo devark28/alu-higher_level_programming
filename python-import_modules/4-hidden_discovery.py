98ebca7863880be22364077083981fa8fb850923
devark28 <e.shimwa1@alustudent.com>
2024-10-23 12:12:45 +0200

    Make 4-hidden_discovery.py Wed Oct 23 12:12:45 SAST 2024 executable

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import importlib.machinery as mach
-import os
-
 
 if __name__ == "__main__":
     module_name = 'hidden_4'
     module_file = './hidden_4.pyc'
     hidden_module = mach.SourcelessFileLoader(module_name, module_file).load_module()
     names = dir(hidden_module)
-    for name in names:
-        print(name)
+        print(names)
+    # for name in names:
         # if not name.startswith('__'):
