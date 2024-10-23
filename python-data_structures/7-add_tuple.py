6803768c9c8386d95011fabb2d6804dce6b3680a
devark28 <e.shimwa1@alustudent.com>
2024-10-23 18:02:31 +0200

    Make 7-add_tuple.py Wed Oct 23 18:02:31 SAST 2024 executable

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,8 +4,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     return tuple(
         map(
-            lambda x, y: x + y,
-            tuple_a + (0, 0),
-            tuple_b + (0, 0)
+            lambda x=0, y=0: x + y,
+            tuple_a,
+            tuple_b
             )
         )
