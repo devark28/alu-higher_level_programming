138ad04e67b01c2b19376515f3478273c508a308
devark28 <e.shimwa1@alustudent.com>
2024-10-23 21:25:30 +0200

    Make 10-best_score.py Wed Oct 23 21:25:30 SAST 2024 executable

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,4 +5,4 @@ def best_score(a_dictionary):
     if a_dictionary:
         max_score = max(a_dictionary.values())
     return None
-best_score({'John': 100, 'Bob': 300, 'Mike': 200, 'Sarah': 500})
+print(best_score({'John': 100, 'Bob': 300, 'Mike': 200, 'Sarah': 500}))
