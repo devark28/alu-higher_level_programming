7dc018ddb3680da2e1bcc89aa58ee43ddaf82c2f
devark28 <e.shimwa1@alustudent.com>
2024-10-23 22:38:20 +0200

    Make 102-complex_delete.py Wed Oct 23 22:38:20 SAST 2024 executable

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,5 @@
 
 def complex_delete(a_dictionary, value):
     if isinstance(a_dictionary, dict) and value in a_dictionary.values():
-        map(lambda x: x, [k for k, v in a_dictionary.items() if v == value])
+        for d in [k for k, v in a_dictionary.items() if v == value]:
+            del a_dictionary[d]
