704610ddd9abd490786046fe43e1913f2179ac9a
devark28 <e.shimwa1@alustudent.com>
2024-10-23 20:50:10 +0200

    Make 0-square_matrix_simple.py Wed Oct 23 20:50:10 SAST 2024 executable

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 
 
 def square_matrix_simple(matrix=[]):
-    return map(lambda x: list(
+    return list(map(lambda x: list(
         map(lambda y: y ** 2, x)
-        ), matrix)
+        ), matrix))
