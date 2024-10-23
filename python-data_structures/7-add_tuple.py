b8dedcfe761ed50df298ab42afcd2dd4446469a2
devark28 <e.shimwa1@alustudent.com>
2024-10-23 19:54:17 +0200

    Make 7-add_tuple.py Wed Oct 23 19:54:17 SAST 2024 executable

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,10 +2,11 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    biggest_tuple_len = len(tuple_a) if len(tuple_a) > len(tuple_b) else len(tuple_b)
-    for i in range(biggest_tuple_len):
-        if i >= len(tuple_a):
-            tuple_a += (0,)
-        if i >= len(tuple_b):
-            tuple_b += (0,)
+    # biggest_tuple_len = len(tuple_a) if len(tuple_a) > len(tuple_b) else len(tuple_b)
+    # biggest_tuple_len = biggest_tuple_len if biggest_tuple_len <= 2 else 2
+    # tuple_c = tuple()
+    # for i in range(2):
+    #     tuple_c[i] = tuple_a[i] if i < len(tuple_a) else 0
+    return list(map(sum, zip(tuple_a, tuple_b)))
+    
 print(add_tuple((1), (1, 2, 3)))
