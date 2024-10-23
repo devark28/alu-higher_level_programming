490628f57c1c8fec3624c193ed5627187f2a7304
devark28 <e.shimwa1@alustudent.com>
2024-10-23 17:42:12 +0200

    Make 11-delete_at.py Wed Oct 23 17:42:12 SAST 2024 executable

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,4 +2,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    return my_list if idx < 0 or idx > my_list.__len__() - 1 else filter(lambda _, i: i != idx, enumerate(my_list))
+    if idx < 0 or idx > my_list.__len__() - 1:
+        return my_list 
+    else:
+        return filter(lambda _, i: i != idx, list(enumerate(my_list)))
