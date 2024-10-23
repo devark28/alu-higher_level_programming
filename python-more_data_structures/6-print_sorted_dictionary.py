d5ca1d528770c5c90f1f960b7d49289533160c1e
devark28 <e.shimwa1@alustudent.com>
2024-10-23 22:01:08 +0200

    Make 6-print_sorted_dictionary.py Wed Oct 23 22:01:08 SAST 2024 executable

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    print(f"{k}: {a_dictionary[k]}" for k in sorted(a_dictionary.keys()))
+    print(f"{k}: {a_dictionary[k]}" for k in list(sorted(a_dictionary.keys())))
