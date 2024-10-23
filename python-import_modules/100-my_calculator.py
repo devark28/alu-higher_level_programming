f03e63a8c5f70a8dbf7270fbbe228a41f6b34941
devark28 <e.shimwa1@alustudent.com>
2024-10-23 13:57:33 +0200

    Make 100-my_calculator.py Wed Oct 23 13:57:33 SAST 2024 executable

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    param_len = len(argv) - 1
+    if param_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    result = None
+    match op:
+        case '+':
+            add(a, b)
+        case '-':
+            sub(a, b)
+        case '*':
+            mul(a, b)
+        case '/':
+            div(a, b)
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    print(f"{a} {op} {b} = {result}")
