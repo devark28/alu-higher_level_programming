226c4041374f92efe66eda644e5ca9e6627f373a
devark28 <e.shimwa1@alustudent.com>
2024-10-23 22:09:28 +0200

    Make 6-print_sorted_dictionary.py Wed Oct 23 22:09:28 SAST 2024 executable

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,5 @@ def print_sorted_dictionary(a_dictionary):
     if isinstance(a_dictionary, dict):
         for k in list(sorted(a_dictionary.keys())):
             print(f"{k}: {a_dictionary[k]}")
-        # print(f"{k}: {a_dictionary[k]}" for k in list(sorted(a_dictionary.keys())))
+        # print(f"{k}: {a_dictionary[k]}" 
+        #   for k in list(sorted(a_dictionary.keys())))
