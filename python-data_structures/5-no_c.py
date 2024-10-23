e6f4e602c79cddb60f1a01c62f507192bbd854a5
devark28 <e.shimwa1@alustudent.com>
2024-10-23 19:36:53 +0200

    Make 5-no_c.py Wed Oct 23 19:36:53 SAST 2024 executable

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@
 def no_c(my_string):
     new_string = ''
     for c in range(my_string.__len__()):
-        if my_string[c] != 'c' and c != 'C':
+        if not (my_string[c] == 'c' or my_string[c] == 'C'):
             new_string += my_string[c]
     return my_string
 print(no_c("School Chicken"))
