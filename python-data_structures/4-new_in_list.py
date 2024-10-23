6367ff1ad244bd1d91da6988442e907c4d3e5522
devark28 <e.shimwa1@alustudent.com>
2024-10-23 17:54:00 +0200

    Make 4-new_in_list.py Wed Oct 23 17:54:00 SAST 2024 executable

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0 or idx > my_list.__len__() - 1:
-      return new_list
+        return new_list
     else:
-      new_list[idx] = element
-      return new_list
+        new_list[idx] = element
+        return new_list
