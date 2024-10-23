392e62784d27bf29d169643c07fa3761e443cfb7
devark28 <e.shimwa1@alustudent.com>
2024-10-23 22:13:38 +0200

    Make 7-update_dictionary.py Wed Oct 23 22:13:38 SAST 2024 executable

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    print(a_dictionary)
     return a_dictionary
 # update_dictionary({'a': 'a', 'b': 'b', 'c': 'c'}, 'a', 'A')
