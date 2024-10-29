a1546bf0008ae53f7be5e85bff965e018d268689
devark28 <e.shimwa1@alustudent.com>
2024-10-29 15:30:50 +0200

    Make 0-safe_print_list.py Tue Oct 29 15:30:50 SAST 2024 executable

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,6 +3,6 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        print("".join(str(my_list[i]) for i in range(x)))
+        print(int("".join(str(my_list[i]) for i in range(x))))
     except Exception as e:
         pass
