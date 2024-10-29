55979a99818165e38dda5ca2ebec4b0e3396419c
devark28 <e.shimwa1@alustudent.com>
2024-10-29 21:42:56 +0200

    Make 2-safe_print_list_integers.py Tue Oct 29 21:42:56 SAST 2024 executable

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        num = ""
+        count = 0
+        for i in range(x):
+            num += str(my_list[i])
+            count += 1
+        print(num)
+        return count
+    except:
+        pass
