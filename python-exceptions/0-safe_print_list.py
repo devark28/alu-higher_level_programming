32bb268b312b2532fd2c8a21190bc985a4b56872
devark28 <e.shimwa1@alustudent.com>
2024-10-29 18:14:24 +0200

    Make 0-safe_print_list.py Tue Oct 29 18:16:08 SAST 2024 executable

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,14 +2,13 @@
 
 
 def safe_print_list(my_list=[], x=0):
+    num = ""
+    count = 0
     try:
-        num = ""
-        count = 0
         for i in range(x):
             num += str(my_list[i])
             count += 1
-        print(num)
-        print(count)
-        return count
     except:
         pass
+    print(num)
+    return count
