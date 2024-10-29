16e39a2d5fc9868b167002cc5e404410d959678c
devark28 <e.shimwa1@alustudent.com>
2024-10-29 16:45:31 +0200

    Make 0-safe_print_list.py Tue Oct 29 16:45:30 SAST 2024 executable

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,6 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        print(int("".join(str(my_list[i]) for i in range(x))))
+        num = ""
+        count = 0
+        for i in range(x):
+            num += str(my_list[i])
+            count += 1
+        print(num)
+        return count
     except Exception as e:
         pass
