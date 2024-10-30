d9eb1c48c8195419bdc72f1bfa0340397e1fd4d3
devark28 <e.shimwa1@alustudent.com>
2024-10-30 12:04:14 +0200

    Make 2-safe_print_list_integers.py Wed Oct 30 12:04:14 SAST 2024 executable

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        print()
-    except:
-        pass
+        except:
+            pass
+    print()
     return count
