698e48930626442bdbc14c6de025a36b0518b0d2
devark28 <e.shimwa1@alustudent.com>
2024-10-30 12:43:54 +0200

    Make 4-list_division.py Wed Oct 30 12:43:54 SAST 2024 executable

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,17 +6,15 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = []
             result.append(my_list_1[i] / my_list_2[i])
-        # except Exception as e:
-        #     result.append(0)
-        #     print(e.args)
-        #     if e.args[0] == 'list index out of range':
-        #         print("out of range")
-        #     elif e.args[0] == 'division by zero':
-        #         print("division by 0")
-            # else:
-                # print("wrong type")
         except ZeroDivisionError:
-            # result.append(0)
+            result.append(0)
             print("division by 0")
+        except Exception as e:
+            result.append(0)
+            print(e.args)
+            if e.args[0] == 'list index out of range':
+                print("out of range")
+            # else:
+            #     print("wrong type")
         finally:
             return result
