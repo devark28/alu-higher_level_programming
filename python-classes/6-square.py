71a1324e8fda13f19925cc8bee173789a5598232
devark28 <e.shimwa1@alustudent.com>
2024-10-30 23:50:15 +0200

    Make 6-square.py Wed Oct 30 23:50:15 SAST 2024 executable

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -35,9 +35,7 @@ class Square:
         self.__size = size
 
     def my_print(self):
-        print(*("\n" * self.__position[1]), sep="", end="")
-        # print(*(" " * self.__position[0]), sep="", end="")
-        # print(*("#" * self.__size for _ in range(self.__size)), sep="\n")
+        print("\n" if self.__position[1] > 0 else "", sep="", end="\n")
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size,
                   end="\n" if i < self.__size - 1 else "")
