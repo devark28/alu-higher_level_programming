1c0170a02c3055dd20917f649b0c0a5fdbd20848
devark28 <e.shimwa1@alustudent.com>
2024-10-30 16:21:57 +0200

    Make 4-square.py Wed Oct 30 16:21:57 SAST 2024 executable

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,15 +8,19 @@ class Square:
     """
     Square class
     """
-    def __init__(self, size=0):
-        if type(size) is not int:
+    def __init__(self, value=0):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
     def area(self):
         return self.__size ** 2
     def size(self):
         return self.__size
     def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
