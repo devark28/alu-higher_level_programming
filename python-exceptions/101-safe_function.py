1385f104d2a2f0cfb2acb0fea380cfab18750dfe
devark28 <e.shimwa1@alustudent.com>
2024-10-30 13:24:02 +0200

    Make 101-safe_function.py Wed Oct 30 13:24:02 SAST 2024 executable

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+from sys import stderr
 
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as e:
-        print("Exception: {}".format(e))
+        stderr.write("Exception: {}".format(e))
