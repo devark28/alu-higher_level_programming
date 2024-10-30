ea7894146ae14e2501950b073411abcaad7b8184
devark28 <e.shimwa1@alustudent.com>
2024-10-30 21:50:49 +0200

    Make 6-square.py Wed Oct 30 21:50:49 SAST 2024 executable

---
+++ python-classes/6-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+an empty Square class
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def my_print(self):
+        print(*("#" * self.__size for _ in range(self.__size)), sep="\n")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        try:
+            (x, y) = value
+            if type(x) is not int or type(y) is not int:
+                raise TypeError("position must be a tuple of 2 positive integers")
+            self.__position = (x, y)
+        except TypeError:
+            raise TypeError("position must be a tuple of 2 positive integers")
