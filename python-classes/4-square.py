a9384973a4cc6dd248d1e962e102b52ac8bd97a4
devark28 <e.shimwa1@alustudent.com>
2024-10-30 16:20:54 +0200

    Make 4-square.py Wed Oct 30 16:20:54 SAST 2024 executable

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,6 +14,9 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
     def area(self):
         return self.__size ** 2
+    def size(self):
+        return self.__size
+    def size(self, value):
+        self.__size = value
