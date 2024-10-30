664d37fa29822fc283f3e2c997428644e9399463
devark28 <e.shimwa1@alustudent.com>
2024-10-30 13:00:05 +0200

    Make 4-list_division.py Wed Oct 30 13:00:05 SAST 2024 executable

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -22,4 +22,4 @@ def list_division(my_list_1, my_list_2, list_length):
             continue
     return result
 
-print(list_division([10, 0, 4], [2, 4, 0], 3)) # [5.0]
+# print(list_division([10, 0, 4], [2, 4, 0], 3)) # [5.0]
