163973688889753c92b1127f34903459ecdce7c1
devark28 <e.shimwa1@alustudent.com>
2024-10-30 13:25:10 +0200

    Make 100-safe_print_integer_err.py Wed Oct 30 13:25:10 SAST 2024 executable

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,8 +3,10 @@ from sys import stderr
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(value), end="")
         return True
     except Exception as e:
         stderr.write("Exception: {}".format(e))
         return False
+    finally:
+        print()
