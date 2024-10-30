eafcc187caf15539d0ad26bf7dc9eb529cbaff33
devark28 <e.shimwa1@alustudent.com>
2024-10-30 22:48:09 +0200

    Make 6-square.py Wed Oct 30 22:46:25 SAST 2024 executable

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,13 +8,13 @@ class Square:
     """
     Square class
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        self.__position
+        self.__position = position
 
     def area(self):
         return self.__size ** 2
