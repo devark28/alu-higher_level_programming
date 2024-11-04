0794de46a93e37a4b84d5c478c665ce011d5a6e5
devark28 <e.shimwa1@alustudent.com>
2024-11-04 22:30:50 +0200

    Make 101-nqueens.py Mon 04 Nov 2024 10:30:50 PM CAT executable

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -36,16 +36,16 @@ def solve_n_queens(n, row=0, board=[]):
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
 
     try:
         N = int(sys.argv[1])
         if N < 4:
             print("N must be at least 4")
-            sys.exit(1)
+            exit(1)
     except ValueError:
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
 
     solutions = []
     solve_n_queens(N)
