1ed2617b336a4d865a886c51b9428a2119849e23
devark28 <e.shimwa1@alustudent.com>
2024-11-04 12:33:06 +0200

    Make 6-rectangle.py Mon 04 Nov 2024 12:33:06 PM CAT executable

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -2,13 +2,13 @@
 """
 an empty Rectangle class
 """
+number_of_instances = 0  # number of instances of Rectangle
 
 
 class Rectangle:
     """
     Rectangle class
     """
-    number_of_instances = 0  # number of instances of Rectangle
     def __init__(self, width=0, height=0):
         """
         initializes the Rectangle class
