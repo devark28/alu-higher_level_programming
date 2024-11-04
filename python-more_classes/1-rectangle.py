a6ce97f7feaa5ab63571636fc42e234f00eb59be
devark28 <e.shimwa1@alustudent.com>
2024-11-04 11:42:06 +0200

    Make 1-rectangle.py Mon 04 Nov 2024 11:42:06 AM CAT executable

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,4 +8,37 @@ class Rectangle:
     """
     Rectangle class
     """
-    pass
+    def __init__(self, width=0, height=0):
+        """
+        initializes the Rectangle class
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        returns the width of the Rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        sets the width of the Rectangle
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        returns the height of the Rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        sets the height of the Rectangle
+        """
+        self.__height = value
