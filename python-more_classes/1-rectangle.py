bfb83eb8d06d59e6c10bdd48f4e51ab15fec59e6
devark28 <e.shimwa1@alustudent.com>
2024-11-04 11:52:49 +0200

    Make 1-rectangle.py Mon 04 Nov 2024 11:52:49 AM CAT executable

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,8 +12,16 @@ class Rectangle:
         """
         initializes the Rectangle class
         """
-        self.__width = width
-        self.__height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.height = height
 
     @property
     def width(self):
