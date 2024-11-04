0d97f5c965693092e4f3fbae6a149aac717acdae
devark28 <e.shimwa1@alustudent.com>
2024-11-04 13:01:52 +0200

    Make 7-rectangle.py Mon 04 Nov 2024 01:01:52 PM CAT executable

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -83,7 +83,9 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width for i in range(self.__height)])
+        return "\n".join([
+            str(self.print_symbol) * self.__width for i in range(self.__height)
+            ])
 
     def print(self):
         """
