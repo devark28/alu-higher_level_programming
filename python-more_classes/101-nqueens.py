2cb5239edb7e1a91a9a69eb3fb612ae616b6e8fb
devark28 <e.shimwa1@alustudent.com>
2024-11-05 11:25:46 +0200

    Make 101-nqueens.py Tue 05 Nov 2024 11:25:46 AM CAT executable

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,45 +1,37 @@
 #!/usr/bin/python3
 import sys
 
-def print_solutions(solutions):
-    """
-    Print the solutions of the N-Queens problem
-    """
-    for solution in solutions:
-        print(solution)
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
 
-def is_safe(board, row, col):
-    """
-    Check if a queen can be placed in a given position
-    """
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
-            return False
-    return True
+class Point:
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
 
-def solve_n_queens(n, row=0, board=[]):
-    """
-    Solve the N-Queens problem
-    """
-    if row == n:
-        solutions.append(board.copy())
-        return
-    
-    for col in range(n):
-        if is_safe(board, row, col):
-            board.append(col)
-            solve_n_queens(n, row + 1, board)
-            board.pop()
+    def get_displacement(self, point: 'Point') -> Vector:
+        return Vector(point.__x - self.__x, point.__y - self.__y)
+
+class Tree:
+    def __init__(self, max_children):
+        self.__max_children = max_children
+        self.__nodes_pool = [Node()]
+
+class Node:
+    def __init__(self, n):
+        pass
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    argv = sys.argv
+    argc = len(argv)
+    if argc != 2:
         print("Usage: nqueens N")
         exit(1)
 
     try:
-        N = int(sys.argv[1])
+        N = int(argv[1])
         if N < 4:
             print("N must be at least 4")
             exit(1)
@@ -47,6 +39,4 @@ if __name__ == "__main__":
         print("N must be a number")
         exit(1)
 
-    solutions = []
-    solve_n_queens(N)
-    print_solutions(solutions)
+    tree = Tree(max_children=8)
