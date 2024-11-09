655a4e0623f50fb06fcb7ceae698bd0a2f32475d
devark28 <e.shimwa1@alustudent.com>
2024-11-09 16:18:02 +0200

    task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -29,3 +29,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         super().integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Calculates the area of the Rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the Rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
