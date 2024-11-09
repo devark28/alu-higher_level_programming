c578eeb894215dccaa563043746f744a3f322bfc
devark28 <e.shimwa1@alustudent.com>
2024-11-09 15:17:27 +0200

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if obj is an instance of a class that inherited from
+    a_class"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
