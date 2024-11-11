fa9d18e22107132ec0d2672d49da1da1f1daaff1
devark28 <e.shimwa1@alustudent.com>
2024-11-11 12:02:53 +0200

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,10 +4,5 @@
 
 def read_file(filename=""):
     """Read a text file and print it to stdout."""
-    try:
-        with open(filename, "r") as f:
-            print(f.read(), end="")
-    # except FileNotFoundError:
-    #     pass
-    except PermissionError:
-        pass
+    with open(filename, "r") as f:
+        print(f.read(), end="")
