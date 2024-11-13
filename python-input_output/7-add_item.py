2a3bf4f413bbc700e50538b68ebc73987144411b
devark28 <e.shimwa1@alustudent.com>
2024-11-13 22:20:19 +0200

    i/o task

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    data = load_from_json_file("add_item.json")
+    data = data + args
+    save_to_json_file("add_item.json")
