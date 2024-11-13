4fe8a3802a2e496515ae3bd889486633a67fab82
devark28 <e.shimwa1@alustudent.com>
2024-11-13 22:39:44 +0200

    i/o task

---
+++ python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+class_to_json = __import__("8-class_to_json").class_to_json
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        class_to_json(self)
