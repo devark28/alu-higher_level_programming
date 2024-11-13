71c6185455caa0162d589686b8bf242782630dc8
devark28 <e.shimwa1@alustudent.com>
2024-11-13 22:23:43 +0200

    i/o task

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+
+
+def class_to_json(obj):
+    """Return the dictionary description with simple data structure."""
+    props = dir(obj)
+    dict_property = {}
+    for p in props:
+        dict_property[p] = obj[p]
+    return dict_property
