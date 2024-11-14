7f7f8704bae391788bc23b88a7a1cb0a9b14f460
devark28 <e.shimwa1@alustudent.com>
2024-11-14 11:31:36 +0200

    i/o task

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Print Pascal's Triangle."""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    result = []
+    for i in range(n):
+        res = [1]
+        for j in range(i):
+            if j > 0:
+                res.append(res[j - 1] + res[j])
+        res.append(1)
+        result.append(res)
