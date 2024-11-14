5ee23f036c8264922d2e5708b7396feca0d5a85f
devark28 <e.shimwa1@alustudent.com>
2024-11-14 11:50:34 +0200

    i/o task

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,13 +4,18 @@
 
 def pascal_triangle(n):
     """Print Pascal's Triangle."""
+    row = []
     if n <= 0:
-        return []
+        return row
+    column = []
     for i in range(0, n):
         row = [1]
         if i > 0:
             for j in range(1, i):
-                row.append(pascal_triangle(n - 1)[i - 1][j - 1] +
-                           pascal_triangle(n - 1)[i - 1][j])
+                row.append(column[i - 1][j - 1] + column[i - 1][j])
             row.append(1)
-        print(" ".join(str(x) for x in row))
+        column.append(row)
+    return column
+
+# if __name__ == "__main__":
+#     print(pascal_triangle(5))
