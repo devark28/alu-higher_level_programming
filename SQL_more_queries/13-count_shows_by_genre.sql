a8aa281122caa0d939bf9a033292e6a75c643796
devark28 <e.shimwa1@alustudent.com>
2024-11-16 22:42:13 +0200

    more sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,6 @@
 --
-SELECT DISTINCT tv_genres.name AS genre, (
-    SELECT COUNT(*)
-    FROM tv_show_genres
-    WHERE tv_genres.id = tv_show_genres.genre_id
-) AS number_of_shows
+SELECT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres, tv_show_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
