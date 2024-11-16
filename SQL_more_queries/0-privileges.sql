93b2a812761f576e0d1d069f95fb91f14abd5f31
devark28 <e.shimwa1@alustudent.com>
2024-11-16 19:56:34 +0200

    more sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- list for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- list for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
