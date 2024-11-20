33da4ddaf5aff0b26287cedf836f58a38c53e5a8
devark28 <e.shimwa1@alustudent.com>
2024-11-20 22:21:28 +0200

    Make 1-body.sh Wed 20 Nov 2024 10:21:28 PM CAT executable

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument, and displays only the body of 200 status code responses.
-curl -s "$1" | awk '/200/ {print $0}'
+curl -siL "$1" | tail -n -1
