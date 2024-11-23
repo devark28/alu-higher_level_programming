ae8fd2cf1035e01b6bbec9aa3e05ac96d5e8f768
devark28 <e.shimwa1@alustudent.com>
2024-11-23 23:25:51 +0200

    network 1 task

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-takes letter, sends POST request with letter,
-displays body of response
+takes letter, sends POST request with letter, displays body of response
 """
 from sys import argv
 
 from requests import JSONDecodeError, post
 
 if __name__ == "__main__":
-    """
-    takes letter, sends POST request with letter,
-    """
     url = "http://0.0.0.0:5000/search_user"
     letter = ""
     if len(argv) > 1:
@@ -19,6 +15,9 @@ if __name__ == "__main__":
     response = post(url, data=data)
 
     try:
+        """
+        If the JSON is empty, print No result
+        """
         json = response.json()
         if json:
             print("[{}] {}".format(json.get("id"), json.get("name")))
