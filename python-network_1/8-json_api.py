82b341094e0262a55e327ada9269f5209c3e991b
devark28 <e.shimwa1@alustudent.com>
2024-11-23 23:12:17 +0200

    network 1 task

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+takes letter, sends POST request with letter, displays body of response
+"""
+from sys import argv
+
+import requests
+from requests import JSONDecodeError
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    letter = ""
+    if len(argv) > 1:
+        letter = argv[1]
+    data = {"q": letter}
+    response = requests.post(url, data=data)
+
+    try:
+        json = response.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+    except JSONDecodeError:
+        print("Not a valid JSON")
