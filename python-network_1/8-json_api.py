02da0608a2c0c849e5f47c689a4f15827130c40d
devark28 <e.shimwa1@alustudent.com>
2024-11-23 23:23:47 +0200

    network 1 task

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -8,6 +8,9 @@ from sys import argv
 from requests import JSONDecodeError, post
 
 if __name__ == "__main__":
+    """
+    takes letter, sends POST request with letter,
+    """
     url = "http://0.0.0.0:5000/search_user"
     letter = ""
     if len(argv) > 1:
