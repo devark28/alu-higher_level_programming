b828ba76062d2d4c3e10ccd1fcf1023982bb8606
devark28 <e.shimwa1@alustudent.com>
2024-11-23 16:26:47 +0200

    network 1 task

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,6 +6,5 @@ from sys import argv
 from urllib import request
 
 if __name__ == "__main__":
-    url = argv[1]
-    with request.urlopen(url) as response:
+    with request.urlopen(argv[1]) as response:
         print(response.info().get("X-Request-Id"))
