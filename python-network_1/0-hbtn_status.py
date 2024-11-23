d0bc561c65fc785d9e1427a570f8da2ce4b7fb25
devark28 <e.shimwa1@alustudent.com>
2024-11-23 16:08:15 +0200

    network 1 task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ Python script that fetches https://alu-intranet.hbtn.io/status
 from urllib import request
 
 if __name__ == "__main__":
-    with request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
         print("Body response:")
         print("\t- type:", type(html))
