9d69565ddfe971e6baea6339df11b739ab24762e
devark28 <e.shimwa1@alustudent.com>
2024-11-23 23:39:20 +0200

    network 1 task

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -4,9 +4,9 @@ fetches https://api.github.com/user and displays id
 """
 from sys import argv
 
-from requests import get, auth
+from requests import get
 
 if __name__ == "__main__":
     url = "https://api.github.com/user"
-    response = get(url, auth=auth.HTTPBasicAuth(argv[1], argv[2]))
+    response = get(url, auth=(argv[1], argv[2]))
     print(response.json().get("id"))
