78feaa0b43fda837d01fbbaa9d4b1d4b1b2a6932
devark28 <e.shimwa1@alustudent.com>
2024-11-23 16:11:52 +0200

    network 1 task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,7 +6,10 @@ Python script that fetches https://alu-intranet.hbtn.io/status
 from urllib import request
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
+    with request.urlopen(
+            "https://alu-intranet.hbtn.io/status"
+            if "https://intranet.hbtn.io/status".startswith("https")
+            else "https://intranet.hbtn.io/status") as response:
         html = response.read()
         print("Body response:")
         print("\t- type:", type(html))
