cf068dc6243a8373b425ddb2b58bfeda87b7ec8b
devark28 <e.shimwa1@alustudent.com>
2024-11-24 23:34:52 +0200

    test driven dev task

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function add integer
+"""
+
+
+def add_integer(a, b=98):
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    print(add_integer(1.1))
