5f2782ff74cec66b0140be015027748d9af3e6f1
devark28 <e.shimwa1@alustudent.com>
2024-11-24 23:50:06 +0200

    test driven dev task

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,6 +5,9 @@ Function add integer
 
 
 def add_integer(a, b=98):
+    """
+    Function that adds 2 numbers
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
