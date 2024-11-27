b96445f6ed8fefd9dd24ab47ba9f8d7e51f8ef8f
devark28 <e.shimwa1@alustudent.com>
2024-11-27 23:07:52 +0200

    test driven dev task

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -12,7 +12,7 @@ class RectangleTest(unittest.TestCase):
     Test Rectangle class
     """
 
-    def test_width_with_2(self):
+    def test_rect_with_2(self):
         """
         Test width
         """
@@ -22,7 +22,7 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(r.x, 0)
         self.assertEqual(r.y, 0)
 
-    def test_width_with_3(self):
+    def test_rect_with_3(self):
         """
         Test width
         """
@@ -32,7 +32,7 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(r.x, 3)
         self.assertEqual(r.y, 0)
 
-    def test_width_with_4(self):
+    def test_rect_with_4(self):
         """
         Test width
         """
@@ -41,6 +41,29 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(r.height, 2)
         self.assertEqual(r.x, 3)
         self.assertEqual(r.y, 4)
+
+    def test_rect_type_errors(self):
+        """
+        Test width
+        """
+        with self.assertRaises(TypeError):
+            r = Rectangle("1", 2)
+
+        with self.assertRaises(TypeError):
+            r = Rectangle(1, "2")
+
+        with self.assertRaises(TypeError):
+            r = Rectangle(1, 2, "3")
+
+        with self.assertRaises(TypeError):
+            r = Rectangle(1, 2, 3, "4")
+
+    def test_rect_with_5_args(self):
+        """
+        Test width
+        """
+        with self.assertRaises(TypeError):
+            r = Rectangle(1, 2, 3, 4, 5)
 
 
 if __name__ == '__main__':
