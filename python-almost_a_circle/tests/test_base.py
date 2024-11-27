025a9743829ef9d5d704e297552863c1e6460ffe
devark28 <e.shimwa1@alustudent.com>
2024-11-27 21:56:18 +0200

    test driven dev task

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -5,6 +5,7 @@ Test Base class
 import unittest
 
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class BaseTest(unittest.TestCase):
@@ -36,6 +37,40 @@ class BaseTest(unittest.TestCase):
         b2 = Base(None)
         b3 = Base(None)
         self.assertEqual(b1.id, b3.id - 2)
+
+    def test_to_json_string(self):
+        """
+        Test to_json_string
+        """
+
+        r1 = Rectangle(10, 7, 8, 6)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(type(json_dictionary), str)
+        self.assertEqual(len(json_dictionary), 53)
+
+    def test_to_json_string_empty(self):
+        """
+        Test to_json_string empty
+        """
+        json_dictionary = Base.to_json_string([])
+        self.assertEqual(json_dictionary, "[]")
+
+    def test_to_json_string_None(self):
+        """
+        Test to_json_string None
+        """
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, "[]")
+
+    def test_to_json_string_single_dict(self):
+        """
+        Test to_json_string single dict
+        """
+        dict_input = [{'id': 12}]
+        json_output = Base.to_json_string(dict_input)
+        self.assertEqual(json_output, '[{"id": 12}]')  # Assuming the expected JSON string format
+        self.assertIsInstance(json_output, str)
 
 
 if __name__ == '__main__':
