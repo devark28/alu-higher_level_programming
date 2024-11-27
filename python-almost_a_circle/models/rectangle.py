460f77a0054ad848c76761944f22b8e5fbf58450
devark28 <e.shimwa1@alustudent.com>
2024-11-27 17:40:06 +0200

    test driven dev task

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+Rectangle module
+"""
+
+from base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructor
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """
+        Getter for width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        """
+        Setter for width
+        """
+        self.__width = w
+
+    @property
+    def height(self):
+        """
+        Getter for height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, h):
+        """
+        Setter for height
+        """
+        self.__height = h
+
+    @property
+    def x(self):
+        """
+        Getter for x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """
+        Setter for x
+        """
+        self.__x = x
+
+    @property
+    def y(self):
+        """
+        Getter for y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """
+        Setter for y
+        """
+        self.__y = y
