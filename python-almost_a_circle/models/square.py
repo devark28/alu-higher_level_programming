ff452755d125881d14f3f153147b1f29ec157855
devark28 <e.shimwa1@alustudent.com>
2024-11-27 20:37:47 +0200

    test driven dev task

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -62,3 +62,14 @@ class Square(Rectangle):
                 self.x = kwargs['x']
             if 'y' in kwargs:
                 self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """
+        Return dictionary
+        """
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
