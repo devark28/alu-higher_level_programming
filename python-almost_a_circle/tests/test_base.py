8037a831399595fd6b2d77292750671bc52e46b9
devark28 <e.shimwa1@alustudent.com>
2024-11-27 22:03:17 +0200

    test driven dev task

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -61,7 +61,7 @@ class BaseTest(unittest.TestCase):
         Test to_json_string None
         """
         json_dictionary = Base.to_json_string(None)
-        self.assertEqual(json_dictionary, "[]")
+        self.assertEqual("[]", json_dictionary)
 
     def test_to_json_string_single_dict(self):
         """
