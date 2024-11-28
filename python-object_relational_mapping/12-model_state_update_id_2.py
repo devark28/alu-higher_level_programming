5c7062d7d1dbf34605b57a3d31fd8e9ed1f9710e
devark28 <e.shimwa1@alustudent.com>
2024-11-29 00:31:16 +0200

    orm task

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     state = session.query(State).filter(State.id == 2).first()
-    session.name = "New Mexico"
+    state.name = "New Mexico"
     session.commit()
     # print(f"{state.id}")
 
