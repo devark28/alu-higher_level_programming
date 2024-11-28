ba2d03a36a0a8bfb260779c3d56466d19062ecf4
devark28 <e.shimwa1@alustudent.com>
2024-11-28 22:56:09 +0200

    orm task

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     )
     session = Session(engine)
 
-    for state in session.query(State).order_by(State.id):
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
