9de5483202f0cc645d0f47dcf2c0633ee25c6506
devark28 <e.shimwa1@alustudent.com>
2024-11-28 21:55:38 +0200

    orm task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,11 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    username, password, database = argv[0:3]
+    username, password, database = argv[1:4]
     # default host is 'localhost' and default port is '3306'
     connection = MySQLdb.connect(
         user=username,
-        passwd=password,
+        password=password,
         db=database
     )
 
