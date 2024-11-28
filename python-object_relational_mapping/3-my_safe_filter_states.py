f81de35c4a83f1e1af9ebfd033763bed671161c9
devark28 <e.shimwa1@alustudent.com>
2024-11-28 22:42:56 +0200

    orm task

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     cursor = connection.cursor()
     cursor.execute(
         'SELECT * FROM states '
-        'WHERE name = "{}" '
-        'ORDER BY states.id'.format(search_name))
+        'WHERE name = %s '
+        'ORDER BY states.id', (search_name,))
     states = cursor.fetchall()
 
     for state in states:
