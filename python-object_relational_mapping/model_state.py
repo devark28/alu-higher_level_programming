4b1393714012a67bf449bae2eeb3dbf975c6aa50
devark28 <e.shimwa1@alustudent.com>
2024-11-28 23:42:25 +0200

    orm task

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -16,5 +16,6 @@ class State(Base):
 
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
