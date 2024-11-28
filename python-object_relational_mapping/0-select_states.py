2354d0a546b34bd3b25ad1dd7baf466df6afdd88
devark28 <e.shimwa1@alustudent.com>
2024-11-28 21:49:10 +0200

    orm task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,3 +2,24 @@
 """
 Select all records from states table
 """
+from sys import argv
+
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, database = argv[0:3]
+    # default host is 'localhost' and default port is '3306'
+    connection = MySQLdb.connect(
+        user=username,
+        password=password,
+        db=database
+    )
+
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY states.id')
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    connection.close()
