170770e67888c0347885f30ef0c5f7c7e059a422
devark28 <e.shimwa1@alustudent.com>
2025-01-20 17:37:33 +0200

    UPD: task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (argv.length === 0) {
+if ([...process.argv].length === 0) {
     console.log('No argument');
-} else if (argv.length === 1) {
+} else if ([...process.argv].length === 1) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
