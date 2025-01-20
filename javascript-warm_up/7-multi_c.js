af65a095e9f2cf4c17ea60f0d431e8d192dc8ab9
devark28 <e.shimwa1@alustudent.com>
2025-01-20 21:32:27 +0200

    Make changes to 7-multi_c on Mon Jan 20 09:32:27 PM CAT 2025

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+if (Number.isNaN(Number(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  let times = Number(process.argv[2]).toFixed(0);
+
+  for (let t = 0; t < times; t++) {
+    console.log('curr');
+  }
+}
