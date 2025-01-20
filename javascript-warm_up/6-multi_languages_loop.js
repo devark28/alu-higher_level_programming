eba7f04af1d3cc92c6c3e3414e0573a554a65c22
devark28 <e.shimwa1@alustudent.com>
2025-01-20 21:11:18 +0200

    Make 6-multi_languages_loop Mon Jan 20 09:11:18 PM CAT 2025 executable

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-let strs = ['C is fun', 'Python is cool']
+let strs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let curr = null;
+while (curr = strs.shift()) {
+  console.log(curr);
+}
