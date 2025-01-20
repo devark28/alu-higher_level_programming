b7b21e7a0050cb34842abb14df53985f95c027fa
devark28 <e.shimwa1@alustudent.com>
2025-01-20 20:57:58 +0200

    Make 5-to_integer Mon Jan 20 08:57:58 PM CAT 2025 executable

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (Number.isNaN(Number(process.argv[2]))) {
-  console.log(process.argv);
+  console.log('Not a number');
 } else {
   console.log(`My number: ${Number(process.argv[2]).toFixed(0)}`);
 }
