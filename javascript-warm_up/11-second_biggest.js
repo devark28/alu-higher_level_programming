85ada7ba46321a0d8137fd28120f95ccd1653a2d
devark28 <e.shimwa1@alustudent.com>
2025-01-21 17:50:51 +0200

    Make changes to 11-second_biggest on Tue Jan 21 05:50:51 PM CAT 2025

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-function getNumberArgs() {
+function getArgs() {
   const scriptArgs = process.argv.slice(2);
   return scriptArgs.length === 0 ? undefined : scriptArgs;
 }
 
-
+function getSecongLargest(elements = []) {
+  return elements.map(Number).toSorted((a, b) => a - b).at(1) ?? 0
+}
