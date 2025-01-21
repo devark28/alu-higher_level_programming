94ef8ff6824109f04a8084538c4ef9f3ea889743
devark28 <e.shimwa1@alustudent.com>
2025-01-21 17:37:37 +0200

    Make changes to 10-factorial on Tue Jan 21 05:37:37 PM CAT 2025

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+    return factorial(n - 1) * n;
+  }
+}
 
-const nums = Number.isNaN(Number(process.argv[2])) ? 0 : Number(process.argv[2]);
-console.log();
+const num = Number.isNaN(Number(process.argv[2])) ? 0 : Number(process.argv[2]);
+console.log(factorial(num));
