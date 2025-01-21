4b4522ed0bf9993e18d43242d05c966b3b8297c1
devark28 <e.shimwa1@alustudent.com>
2025-01-21 17:23:02 +0200

    Make changes to 9-add on Tue Jan 21 05:23:02 PM CAT 2025

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-if (Number.isNaN(Number(process.argv[2]))) {
-  console.log('Missing size');
-} else {
-  const n1 = Number(process.argv[2]);
-  const n2 = Number(process.argv[3]);
-  console.log(add(n1, n2));
-}
+const n1 = Number(process.argv[2]);
+const n2 = Number(process.argv[3]);
+console.log(add(n1, n2));
 
 function add(a, b) {
   return a + b;
