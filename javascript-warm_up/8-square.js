8556a2fcdb7ebdc77fc45300f858a8c87857a4c1
devark28 <e.shimwa1@alustudent.com>
2025-01-21 17:11:36 +0200

    Make changes to 8-square on Tue Jan 21 05:11:36 PM CAT 2025

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (Number.isNaN(Number(process.argv[2]))) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   const times = Number(process.argv[2]).toFixed(0);
   for (let t = 0; t < times; t++) {
