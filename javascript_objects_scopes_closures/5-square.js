f5067a42479314a6845944042eeb7800eb8aed51
devark28 <e.shimwa1@alustudent.com>
2025-01-22 21:02:55 +0200

    Make changes to 5-square on Wed Jan 22 09:02:55 PM CAT 2025

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('4-rectangle');
+const Rectangle = require('javascript_objects_scopes_closures/4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
