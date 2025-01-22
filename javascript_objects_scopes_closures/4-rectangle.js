69cbbec1b0510232b7474d54ebddc7ebb877d94b
devark28 <e.shimwa1@alustudent.com>
2025-01-22 20:52:30 +0200

    Make changes to 4-rectangle on Wed Jan 22 08:52:30 PM CAT 2025

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -15,5 +15,10 @@ class Rectangle {
   rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 module.exports = Rectangle;
