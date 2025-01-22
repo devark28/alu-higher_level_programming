167990ff0a4d761aa68d50f45090481d80eb7b2f
devark28 <e.shimwa1@alustudent.com>
2025-01-22 20:49:20 +0200

    Make changes to 2-rectangle on Wed Jan 22 08:49:20 PM CAT 2025

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if(!w || !h || w <= 0 || h <= 0) return;
+    if (!w || !h || w <= 0 || h <= 0) return;
     this.width = w;
     this.height = h;
   }
 
-  print (){
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
