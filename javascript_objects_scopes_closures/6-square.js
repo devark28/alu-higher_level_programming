3dec03135cee54f967034aca2a544db22d9b4efb
devark28 <e.shimwa1@alustudent.com>
2025-01-22 21:22:03 +0200

    Make changes to 6-square on Wed Jan 22 09:22:03 PM CAT 2025

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const Sq = require('./5-square');
 class Square extends Sq {
   charPrint (c) {
     if (c) {
-      console.log(`${c.repeat(this.width)}\n`.repeat(this.height));
+      console.log(`${c.repeat(this.width)}\n`.repeat(this.height).trimEnd());
     } else {
       super.print();
     }
