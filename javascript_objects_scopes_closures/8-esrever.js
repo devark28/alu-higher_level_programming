0862790795fd3eef448fd74b5ba48a446651d59c
devark28 <e.shimwa1@alustudent.com>
2025-01-22 21:23:45 +0200

    Make changes to 8-esrever on Wed Jan 22 09:23:45 PM CAT 2025

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 module.exports = {
-  nbOccurences: (list, searchElement) => {
+  nbOccurences: (list) => {
     return list.reduce((acc, element) => element === searchElement ? ++acc : acc, 0);
   }
 };
