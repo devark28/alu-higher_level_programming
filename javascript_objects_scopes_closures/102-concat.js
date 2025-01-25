14386ea5e9998cc598ca60d1046ba5dec9a3a80f
devark28 <e.shimwa1@alustudent.com>
2025-01-25 22:12:52 +0200

    Make changes to 102-concat on Sat Jan 25 10:12:52 PM CAT 2025

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const [,,fileA,fileB,fileC] = process.argv;
-console.log(fileA, fileB, fileC);
+const [, , fileA, fileB, fileC] = process.argv;
+const fileAData = require(`./${fileA}`);
+const fileBData = require(`./${fileB}`);
+const fs = require('fs');
+fs.writeFileSync(fileC, JSON.stringify({...fileAData, ...fileBData}));
