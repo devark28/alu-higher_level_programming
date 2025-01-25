b72e85804b9d5529f01b9e4dd65725ac85fea450
devark28 <e.shimwa1@alustudent.com>
2025-01-25 20:06:25 +0200

    Make changes to 101-sorted on Sat Jan 25 08:06:25 PM CAT 2025

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const { dict } = require('./101-data');
-console.log(dict);
 console.log(Object.entries(dict).reduce((acc, [key, val]) => ({
   ...acc,
   [val]: acc[val]?.length ? acc[val].concat(key) : [key]
