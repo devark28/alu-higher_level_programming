6948ef9e9a23a260d8412b9b7b96032df1e00a3b
devark28 <e.shimwa1@alustudent.com>
2025-02-03 20:18:47 +0200

    Make changes to 4-starwars_count on Mon Feb  3 08:18:47 PM CAT 2025

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 require('request')(`https://swapi-api.alx-tools.com/api/films/`, (err, res, body) => {
   console.log(JSON.parse(body)?.results?.reduce((count, film) => {
-    if(film?.characters.find((ele) => ele.match(/.*\/18\/$/))){
-      return ++count;
-    }
-    // console.log(film?.characters);
-    return count;
+    return (film?.characters.find((ele) => ele.match(/.*\/18\/$/))) ? ++count : count;
   }, 0));
 });
