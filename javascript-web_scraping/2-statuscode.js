7e7773868f5671a3fb5580c135c3abfacd769953
devark28 <e.shimwa1@alustudent.com>
2025-02-04 17:00:49 +0200

    Make changes to 2-statuscode on Tue Feb  4 05:00:49 PM CAT 2025

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const [, , URL] = process.argv;
 const request = require('request');
-request(URL, (err, res, body) => {
+request(URL, (err, res) => {
   console.log(`code: ${res?.statusCode}`);
 });
