3ba7d1dd5683ed6319ced5e1aafc9cb4dd37e598
devark28 <e.shimwa1@alustudent.com>
2025-02-04 17:07:46 +0200

    Make changes to 5-request_store on Tue Feb  4 05:07:45 PM CAT 2025

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const [, , URL, fileA] = process.argv;
-require('request')(URL, (_, __, body) => {
+require('request')(URL, (_, _, body) => {
   require('fs').writeFileSync(fileA, body, { encoding: 'utf-8' });
 });
