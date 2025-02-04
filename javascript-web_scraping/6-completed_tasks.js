427fc86a8aa46d4244ddb8b6d34747b7d05f4ed8
devark28 <e.shimwa1@alustudent.com>
2025-02-04 16:36:50 +0200

    Make changes to 6-completed_tasks on Tue Feb  4 04:36:50 PM CAT 2025

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,16 +3,6 @@ const [, , URL] = process.argv;
 require('request')(URL, (err, res, body) => {
   // console.log(JSON.parse(body));
   console.log(JSON.parse(body).reduce((acc, usr) => {
-    // console.log(acc);
-    // if(usr?.completed){
-    //   acc[usr?.userId] = (acc[usr?.userId] || 0) + 1;
-    //   // if(acc[usr?.userId]){
-    //   //   acc[usr?.userId] += 1;
-    //   // }else{
-    //   //   acc[usr?.userId] = 1;
-    //   // }
-    // }
-    // return acc;
     return usr?.completed ? {
       ...acc,
       [usr?.userId]: (acc[usr?.userId] || 0) + 1
