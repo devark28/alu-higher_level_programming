75f71446d96b7b0607d68383945c0662631f7af1
devark28 <e.shimwa1@alustudent.com>
2025-02-04 17:08:26 +0200

    Make changes to 4-starwars_count on Tue Feb  4 05:08:26 PM CAT 2025

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const [, , URL] = process.argv;
-require('request')(URL, (err, res, body) => {
+require('request')(URL, (_, __, body) => {
   console.log(JSON.parse(body)?.results?.reduce((count, film) => {
     return (film?.characters.find((ele) => ele.match(/.*\/18\/$/))) ? ++count : count;
   }, 0));
