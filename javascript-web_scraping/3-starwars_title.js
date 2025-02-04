7d99237bcf3294b49ae366d6e60ba0c1e3770017
devark28 <e.shimwa1@alustudent.com>
2025-02-04 17:02:49 +0200

    Make changes to 3-starwars_title on Tue Feb  4 05:02:49 PM CAT 2025

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,5 +2,5 @@
 const [, , id] = process.argv;
 const request = require('request');
 request(`https://swapi-api.alx-tools.com/api/films/${id}`, (err, res, body) => {
-  // console.log(JSON.parse(body)?.title);
+  console.log(JSON.parse(body)?.title);
 });
