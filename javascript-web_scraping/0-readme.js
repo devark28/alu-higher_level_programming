6650678d0814495a9d0b8fcae160a614f3399518
devark28 <e.shimwa1@alustudent.com>
2025-02-04 16:55:28 +0200

    Make changes to 0-readme on Tue Feb  4 04:55:28 PM CAT 2025

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const [, , fileA] = process.argv;
-console.log(require('fs').readFileSync(fileA,{encoding:'utf-8'}));
+const [, , fileA] = process.argv
+console.log(require('fs').readFileSync(fileA, { encoding: 'utf-8' }));
