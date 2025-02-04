30e7ed302de5852854be815d76bdb6236f12e5bf
devark28 <e.shimwa1@alustudent.com>
2025-02-04 17:11:41 +0200

    Make changes to 101-sorted on Tue Feb  4 05:11:40 PM CAT 2025

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,10 +2,10 @@
 const [, , URL] = process.argv;
 require('request')(URL, (_, __, body) => {
   console.log(JSON.parse(body).reduce((acc, usr) => {
-    return usr?.completed
+    return usr.completed
       ? {
           ...acc,
-          [usr?.userId]: (acc[usr?.userId] || 0) + 1
+          [usr.userId]: (acc[usr.userId] || 0) + 1
         }
       : acc;
   }, {}));
--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -2,5 +2,5 @@
 const { dict } = require('./101-data');
 console.log(Object.entries(dict).reduce((acc, [key, val]) => ({
   ...acc,
-  [val]: acc[val]?.length ? acc[val].concat(key) : [key]
+  [val]: acc[val].length ? acc[val].concat(key) : [key]
 }), {}));
